f1d58f8e43fcac96a2bfa675c921ce7b352a3961
murilob <cristinabritto@gmail.com>
2026-03-04 11:24:42 -0300

    calculamos a media

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-alert ('deu certo'
-)
+const nota1 = document.getElementById("nota1")
+const nota2 = document.getElementById("nota2")
+
+const button = document.getElementById("button")
+
+function calcularMedia(){
+    const valorNota1 = Number(nota1.value)
+    const valorNota2 = Number(nota2.value)
+
+    const media = (valorNota1 + valorNota2)/2  
+}
+
+button.addEventListener("click", calcularMedia)
