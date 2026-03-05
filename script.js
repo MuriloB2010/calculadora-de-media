9b64f371ceac28ecc98c531609605b471024f0d5
murilob <cristinabritto@gmail.com>
2026-03-05 07:48:48 -0300

    finalizamos a média

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const nota1 = document.getElementById("nota1")
 const nota2 = document.getElementById("nota2")
+const resultado = document.getElementById("resultado")
 
 const button = document.getElementById("button")
 
@@ -7,7 +8,9 @@ function calcularMedia(){
     const valorNota1 = Number(nota1.value)
     const valorNota2 = Number(nota2.value)
 
-    const media = (valorNota1 + valorNota2)/2  
+    const media = (valorNota1 + valorNota2)/2 
+    
+    resultado.innerText = `A média é ${media.toFixed(2)}`
 }
 
 button.addEventListener("click", calcularMedia)
